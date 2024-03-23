34138a086f91af9f7829a58bf8e221153d85a5b9
Dhruv Rajvanshi <dhruv@softr.io>
2024-03-23 11:03:09 +0530

    Rebuild vscode extension

--- vscode-hades/client/extension.js
+++ vscode-hades/client/extension.js
@@ -1,8 +1,6 @@
 "use strict";
 exports.__esModule = true;
 exports.deactivate = exports.activate = void 0;
-var vscode_1 = require("vscode");
-var vscode_languageclient_1 = require("vscode-languageclient");
 var client;
 function activate(context) {
 }
