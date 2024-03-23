e43ba1a81d13ec20bc18bbf244762850638729e1
Dhruv Rajvanshi <dhruv@softr.io>
2024-03-23 11:32:42 +0530

    Remove unused code from the vscode extension

--- vscode-hades/client/extension.js
+++ vscode-hades/client/extension.js
@@ -1,14 +1,1 @@
 "use strict";
-exports.__esModule = true;
-exports.deactivate = exports.activate = void 0;
-var client;
-function activate(context) {
-}
-exports.activate = activate;
-function deactivate() {
-    if (!client) {
-        return undefined;
-    }
-    return client.stop();
-}
-exports.deactivate = deactivate;
